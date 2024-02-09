82ea593948b2083f0c032fea6eeae83f727a168f
Laribferraz <laribferraz@gmail.com>
2024-02-09 18:07:41 -0300

    perguntas de direito

--- index.js
+++ index.js
@@ -1,95 +1,96 @@
 const perguntas = [
-    {
-      pergunta: "Qual é a finalidade do comando 'console.log()' em JavaScript?",
-      respostas: [
-        "Exibir uma mensagem de erro",
-        "Imprimir dados no console",
-        "Criar uma variável"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a função do operador '===' em comparações em JavaScript?",
-      respostas: [
-        "Comparação de valores sem considerar o tipo",
-        "Atribuição de valores",
-        "Comparação estrita de valores e tipos"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Como se declara uma variável em JavaScript?",
-      respostas: [
-        "let myVar;",
-        "const myVar = 10;",
-        "ambas as opções acima estão corretas"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "O que é uma função em JavaScript?",
-      respostas: [
-        "Um tipo de dado",
-        "Um bloco de código reutilizável",
-        "Uma variável global"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a diferença entre 'let' e 'const' na declaração de variáveis?",
-      respostas: [
-        "Nenhuma, são sinônimos",
-        "let é usado para valores constantes, const para variáveis",
-        "let permite reatribuição, const cria variáveis imutáveis"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "O que é o DOM em JavaScript?",
-      respostas: [
-        "Um método de criptografia",
-        "Um modelo de objeto para manipular documentos HTML",
-        "Uma linguagem de programação"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Como se realiza uma iteração sobre os elementos de um array em JavaScript?",
-      respostas: [
-        "Usando a estrutura 'if-else'",
-        "Com a declaração 'switch'",
-        "Utilizando loops como 'for' ou 'forEach'"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "O que é o JSON em JavaScript?",
-      respostas: [
-        "Um método de formatação de texto",
-        "Uma linguagem de estilização",
-        "Um formato de dados leve e intercambiável"
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Qual é a diferença entre 'null' e 'undefined' em JavaScript?",
-      respostas: [
-        "São iguais, usados de forma intercambiável",
-        "'null' representa a ausência de valor, 'undefined' é atribuído explicitamente",
-        "Ambos representam valores vazios"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Como se adiciona um evento a um elemento HTML usando JavaScript?",
-      respostas: [
-        "Apenas com CSS",
-        "Usando o atributo 'event'",
-        "Através do método 'addEventListener'"
-      ],
-      correta: 2
-    },
-  ];
+  {
+    pergunta: "O que é o princípio da presunção de inocência?",
+    respostas: [
+      "O réu é considerado culpado até provar sua inocência",
+      "O réu é considerado inocente até que sua culpa seja comprovada",
+      "O réu é automaticamente inocente"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é a função do habeas corpus?",
+    respostas: [
+      "Proteger contra prisões ilegais ou arbitrárias",
+      "Julgar crimes de corrupção",
+      "Garantir a liberdade de expressão"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "O que é a prescrição em direito penal?",
+    respostas: [
+      "É o tempo em que o réu fica preso preventivamente",
+      "É o tempo após o qual uma ação penal não pode mais ser movida",
+      "É o prazo para o réu apresentar sua defesa"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que caracteriza a responsabilidade civil?",
+    respostas: [
+      "A responsabilidade exclusiva do Estado",
+      "A obrigação de indenizar por danos causados a terceiros",
+      "A penalidade por infrações criminais"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é o princípio da legalidade?",
+    respostas: [
+      "A aplicação de penas sem base legal",
+      "A submissão às leis vigentes",
+      "A dispensa de legislação em determinados casos"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é a principal função do Ministério Público?",
+    respostas: [
+      "Defender os interesses do réu",
+      "Promover a justiça e a defesa da ordem jurídica",
+      "Atuar como advogado do governo"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é uma súmula vinculante?",
+    respostas: [
+      "Uma decisão que não obriga os tribunais",
+      "Uma orientação jurisprudencial de caráter obrigatório",
+      "Um parecer sem efeito legal"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "O que é o habeas data?",
+    respostas: [
+      "A garantia de acesso a informações pessoais",
+      "A proibição de prisões preventivas",
+      "A anulação de condenações criminais"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "O que é o direito de representação?",
+    respostas: [
+      "A faculdade de atuar como representante do Ministério Público",
+      "O direito de representar graficamente uma situação",
+      "A possibilidade de denunciar crimes, mesmo sem ser a vítima"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "O que é o devido processo legal?",
+    respostas: [
+      "A garantia de um processo rápido",
+      "O respeito aos direitos fundamentais no decorrer do processo",
+      "A obrigatoriedade de seguir procedimentos não previstos em lei"
+    ],
+    correta: 1
+  }
+];
+
   
   const quiz = document.querySelector('#quiz')
   const template = document.querySelector('template')
